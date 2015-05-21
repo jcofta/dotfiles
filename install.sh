847d027bcf6280cdf59a1cc111f85be6c4cb3ebc
Jan Cofta <jan.cofta@gmail.com>
2015-05-21 03:20:06 +0200

    mod install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-git clone git@github.com:jcofta/dotfiles.git "$HOME/.dotfiles"
-. "$HOME/.dotfiles/bin/dfm install"
+eval $(git clone git@github.com:jcofta/dotfiles.git "$HOME/.dotfiles2")
+eval $( "$HOME/.dotfiles2/bin/dfm install")
