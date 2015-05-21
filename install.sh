278dd86eecde1a6c6b2e42ba74c58c7b3163142e
Jan Cofta <jan.cofta@gmail.com>
2015-05-21 03:34:01 +0200

    repaired install.sh script

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-eval $(git clone git@github.com:jcofta/dotfiles.git "$HOME/.dotfiles2")
-eval $( "$HOME/.dotfiles2/bin/dfm install")
+git clone git@github.com:jcofta/dotfiles.git "$HOME/.dotfiles2"
+"$HOME"/.dotfiles2/bin/dfm install
+
